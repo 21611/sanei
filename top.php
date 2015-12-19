37ff21260f908a9134838d3bddf54bb786c6e2a1
hattori <t00768nh@gmail.com>
2015-12-19 15:15:30 +0900

    test grid

--- top.php
+++ top.php
@@ -5,4 +5,34 @@ Template Name:top
 ?>
 
 
-te
+
+
+<?php get_header(); ?>
+
+
+
+
+<div class="top-container">
+	
+
+<div class="top-left">テスト</div>
+
+
+<div class="top-right">test</div>
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+<?php get_footer(); ?>
